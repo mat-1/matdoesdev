8559f00904e0964d5bfd6fdfa8336bb893e8dca4
mat <github@matdoes.dev>
2022-04-09 00:12:06 +0000

    start switching to markdown-it

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,115 +1,12 @@
-import { marked } from 'marked'
+import MarkdownIt from 'markdown-it'
 
-export function markdownToHtml(md: string, baseUrl?: string): string {
-	const renderer: Partial<marked.Renderer> = {
-		image(href: string, title: string, text: string) {
-			// href = cleanUrl(this.options.sanitize, this.options.baseUrl, href)
-			href = baseUrl ? resolveUrl(baseUrl, href) : href
-			if (href === null) return text
-			let out = `<img src="${href}" alt="${text}"`
-			if (title) out += ` title="${title}"`
-			out += '/>'
-			return out
-		},
-	}
+const md = new MarkdownIt({
+	html: true,
+	breaks: true,
+})
 
-	const centered: marked.TokenizerExtension = {
-		name: 'centered',
-		level: 'block',
-		start(src: string) {
-			// the marked typings want a `number` and we're returning a `number | undefined` so we have to do this :(
-			return src.match(/\|\|/)?.index as number
-		},
-		tokenizer(
-			this: marked.TokenizerThis,
-			src: string,
-			tokens: marked.Token[]
-		): marked.Tokens.Generic | void {
-			const rule = /^\|\|(.+?)\|\|/
-			const match = rule.exec(src)
-			if (match) {
-				const token = {
-					type: 'centered',
-					raw: match[0],
-					text: match[1].trim(),
-					tokens: [],
-				}
-				this.lexer.inline(token.text, token.tokens)
-				return token
-			}
-		},
-		// @ts-expect-error Marked doesn't include `renderer` in the typings.
-		renderer(this: marked.TokenizerThis, token: marked.Tokens): string | false {
-			// @ts-expect-error Property 'parser' does not exist on type 'TokenizerThis'.
-			return `<div class="center">${this.parser.parse(token.tokens)}\n</div>`
-		},
-	}
-	const left: marked.TokenizerExtension = {
-		name: 'left',
-		level: 'block',
-		start(src: string) {
-			// the marked typings want a `number` and we're returning a `number | undefined` so we have to do this :(
-			return src.match(/<-\W/)?.index as number
-		},
-		tokenizer(
-			this: marked.TokenizerThis,
-			src: string,
-			tokens: marked.Token[]
-		): marked.Tokens.Generic | void {
-			const rule = /^<-\W([\w\W]+?)\W<-/
-			const match = rule.exec(src)
-			if (match) {
-				const token = {
-					type: 'left',
-					raw: match[0],
-					text: match[1].trim(),
-					tokens: [],
-				}
-				this.lexer.blockTokens(token.text, token.tokens)
-				return token
-			}
-		},
-		// @ts-expect-error Marked doesn't include `renderer` in the typings.
-		renderer(this: marked.TokenizerThis, token: marked.Tokens): string | false {
-			// @ts-expect-error Property 'parser' does not exist on type 'TokenizerThis'.
-			return `<div class="markdown-float-left">${this.parser.parse(token.tokens)}\n</div>`
-		},
-	}
-	const right: marked.TokenizerExtension = {
-		name: 'right',
-		level: 'block',
-		start(src: string) {
-			// the marked typings want a `number` and we're returning a `number | undefined` so we have to do this :(
-			return src.match(/->\W/)?.index as number
-		},
-		tokenizer(
-			this: marked.TokenizerThis,
-			src: string,
-			tokens: marked.Token[]
-		): marked.Tokens.Generic | void {
-			const rule = /^->\W([\w\W]+?)\W->/
-			const match = rule.exec(src)
-			if (match) {
-				const token = {
-					type: 'right',
-					raw: match[0],
-					text: match[1].trim(),
-					tokens: [],
-				}
-				this.lexer.blockTokens(token.text, token.tokens)
-				return token
-			}
-		},
-		// @ts-expect-error Marked doesn't include `renderer` in the typings.
-		renderer(this: marked.TokenizerThis, token: marked.Tokens): string | false {
-			// @ts-expect-error Property 'parser' does not exist on type 'TokenizerThis'.
-			return `<div class="markdown-float-right">${this.parser.parse(token.tokens)}\n</div>`
-		},
-	}
-
-	marked.use({ renderer, extensions: [centered, left, right] })
-
-	return marked.parse(md, { baseUrl, breaks: true })
+export function markdownToHtml(original: string, baseUrl?: string): string {
+	return md.render(original)
 }
 
 // https://nodejs.org/api/url.html#urlresolvefrom-to
