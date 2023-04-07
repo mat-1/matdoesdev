83b30763c2f68012eefbcf4ac332777c7f3b8e19
mat <git@matdoes.dev>
2023-04-07 18:32:06 -0500

    date as iso string

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -102,7 +102,7 @@ export async function getPost(slug: string): Promise<BlogPost | null> {
 
 	return {
 		title: metadata.title,
-		published: new Date(metadata.published).toString(),
+		published: new Date(metadata.published).toISOString(),
 		html,
 		css,
 		slug,
