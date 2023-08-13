95a1c97822643b5d5c74204d6e5b23b522155fb8
mat <git@matdoes.dev>
2023-08-13 00:56:29 -0500

    link to fediverse in html

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -6,5 +6,7 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
+	
 	<link rel="prefetch" href="/fonts/atkinson-hyperlegible/latin.woff2">
+	<link rel="me" href="https://f.matdoes.dev/mat">
 </svelte:head>
