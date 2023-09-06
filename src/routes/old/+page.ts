a5a49de2f3b997717ae76251a86cb94f2eee5c4d
mat <git@matdoes.dev>
2023-09-05 22:20:14 -0500

    actually prerender old site

--- src/routes/old/+page.ts
+++ src/routes/old/+page.ts
@@ -1,5 +1,7 @@
 import type { Load } from '@sveltejs/kit'
 
+export const prerender = true
+
 export const load: Load = async ({ fetch }) => {
 	const posts = await fetch('/blog.json').then((r: Response) => r.json())
 
