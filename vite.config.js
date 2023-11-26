b7830790d3617a1f7bf48f8a4b94347d5ae09d58
mat <git@matdoes.dev>
2023-11-25 21:11:01 -0600

    serve images in retro page as gifs

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { imagetools } from 'vite-imagetools'
 
 export default {
-	plugins: [
-		sveltekit(),
-	],
+	plugins: [sveltekit(), imagetools()],
 
 	build: {
 		target: 'es2020',
 	},
-	// if the user is on replit or gitpod, use a secure websocket
-	// server:
-	// 	process.env.REPL_ID || process.env.GITPOD_WORKSPACE_ID
-	// 		? {
-	// 			hmr: {
-	// 				protocol: 'wss',
-	// 				port: 443,
-	// 			},
-	// 		}
-	// 		: {},
-
 }
