6abeb86579a14c9a319c42e63f5649b555d4599b
mat <git@matdoes.dev>
2023-11-26 18:37:24 -0600

    move font to

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import atkinsonHyperlegible from '$lib/fonts/atkinson-hyperlegible/latin.woff2'
+
 	export let title = 'matdoesdev'
 	export let description = ''
 </script>
@@ -6,7 +8,7 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	
-	<link rel="prefetch" href="/fonts/atkinson-hyperlegible/latin.woff2">
-	<link rel="me" href="https://f.matdoes.dev/mat">
+
+	<link rel="prefetch" href={atkinsonHyperlegible} />
+	<link rel="me" href="https://f.matdoes.dev/mat" />
 </svelte:head>
