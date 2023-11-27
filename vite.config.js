a1122c65b63b2621464fcfd18c93fce56e497dcf
mat <git@matdoes.dev>
2023-11-26 23:09:00 -0600

    set vite base to ''

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 
+/** @type {import('vite').UserConfig} */
 export default {
 	plugins: [sveltekit()],
 
 	build: {
 		target: 'es2020',
 	},
+
+	base: '',
 }
