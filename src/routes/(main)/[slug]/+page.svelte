2451e294982990e61a44e02fa3722c922842eafb
mat <git@matdoes.dev>
2024-06-24 02:08:10 +0000

    crawl txt and md

--- src/routes/(main)/[slug]/+page.svelte
+++ src/routes/(main)/[slug]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import type { PageData } from "./$types"
+	import type { PageData } from './$types'
 
 	export let data: PageData
 	const { page, slug } = data
 </script>
 
-<!-- make sveltekit crawl to the json api -->
+<!-- make sveltekit crawl to the json, txt, and md api -->
 <div style="display:none"><a href="{slug}.json">JSON API</a></div>
+<div style="display:none"><a href="{slug}.txt">txt</a></div>
+<div style="display:none"><a href="{slug}.md">md</a></div>
 
 <svelte:component this={page} />
