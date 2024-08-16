8d92991f446cf37cec0850a066172b2250d3dfbc
mat <git@matdoes.dev>
2024-08-16 00:24:45 +0000

    new 88x31s

--- src/routes/retro/+page.svelte
+++ src/routes/retro/+page.svelte
@@ -279,6 +279,8 @@
 								/>
 								<Button href="//matrix.org" src="matrix.png" alt="matrix" />
 								<Button href="//caddyserver.com/" src="caddy.png" alt="caddy" />
+								<Button href="//display-a.sand.cat/" src="sandcat.png" alt="display a sand cat" />
+								<Button href="//makea.fish/" src="makeafish.png" alt="make a fish" />
 							</div>
 						</td>
 					</tr>
