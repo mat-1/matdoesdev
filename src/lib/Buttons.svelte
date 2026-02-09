7ea126eb72a87fd548fc0e2508ff4f939ec3d527
mat <git@matdoes.dev>
2026-02-08 18:23:17 -0600

    sage.cat 88x31

--- src/lib/Buttons.svelte
+++ src/lib/Buttons.svelte
@@ -39,6 +39,7 @@
 	<Button href="//timedout.uk" src="nexy.gif" alt="nexy" />
 	<Button href="//paddy.li" src="paddy.gif" alt="paddy" />
 	<Button href="//kathund.dev" src="kath.png" alt="kath" />
+	<Button href="//sage.cat" src="sage.png" alt="sage" />
 	<!-- other -->
 	<Button href="//github.com/mat-1" src="github.gif" alt="github" />
 	<Button href="//ko-fi.com/matdoesdev" src="kofi.gif" alt="kofi" />
